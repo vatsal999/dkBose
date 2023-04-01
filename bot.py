97be5de9840d579ab7893602ddf41bbdd8e51a7c
Rishabh Verma <10rishabhv@gmail.com>
2023-04-01 16:30:19 +0530

    refactor code and fix imgur link case in 'img' command

--- bot.py
+++ bot.py
@@ -43,96 +43,86 @@ async def cuss(ctx, *, user):
 
 @bot.command(help="Displays user avatar")
 async def avatar(ctx, arg: typing.Optional[discord.Member]):
-        if (arg):
-            user = arg
-        else:
-            user = ctx.author
-        embedVar = discord.Embed(title=user.name + "'s Avatar")
+    user = ctx.author
+    if (arg):
+        user = arg
+    embed_msg = discord.Embed(title=user.name + "'s Avatar")
 
-        embedVar.set_image(url=user.avatar.url)
-        embedVar.timestamp = datetime.datetime.now(tz=ZoneInfo('Asia/Kolkata'))
-        embedVar.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar)
-        
-        await ctx.channel.send(embed=embedVar)
-    
+    embed_msg.set_image(url=user.avatar.url)
+    embed_msg.timestamp = datetime.datetime.now(tz=ZoneInfo('Asia/Kolkata'))
+    embed_msg.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar)
+
+    await ctx.channel.send(embed=embed_msg)
+
 
 # lists all the tags available
 @bot.command(help='lists all tags available for !tag <tagname>')
 async def taglist(ctx):
-        f = open('tag.json')
-        taglist = json.loads(f.read())
-        tag_names = ', '.join((i["tag_name"]) for i in taglist["tags"])
+    f = open('tag.json')
+    taglist = json.loads(f.read())
+    tag_names = ', '.join((i["tag_name"]) for i in taglist["tags"])
 
-        embedVar = discord.Embed(title="tags",description=tag_names, color=0x1D2021)
-        embedVar.timestamp = datetime.datetime.utcnow()
+    embed_msg = discord.Embed(title="tags",description=tag_names, color=0x1D2021)
+    embed_msg.timestamp = datetime.datetime.utcnow()
 
-        embedVar.set_footer(text=f"use: {command_prefix}tag <tag>")
+    embed_msg.set_footer(text=f"use: {command_prefix}tag <tag>")
 
-        await ctx.channel.send(embed=embedVar)
+    await ctx.channel.send(embed=embed_msg)
+    f.close()
 
-        #await ctx.channel.send(tag_names)
-        # await ctx.channel.send(taglist["tags"][i]["tag_name"])
-        # await ctx.channel.send(taglist["tag_name"])
-        f.close()
 
 @bot.command(help=f"quotes a msg. usage: {command_prefix}quote <msgID>")
 async def quote(ctx, msgID):
-        msgobj = await ctx.fetch_message(msgID)
-        msg = msgobj.content
+    msgobj = await ctx.fetch_message(msgID)
+    msg = msgobj.content
 
-        embedVar = discord.Embed(title=msgobj.author, description=msg, url=msgobj.jump_url)
-        embedVar.set_thumbnail(url=msgobj.author.avatar.url)
-        embedVar.timestamp = msgobj.created_at
+    embed_msg = discord.Embed(title=msgobj.author, description=msg, url=msgobj.jump_url)
+    embed_msg.set_thumbnail(url=msgobj.author.avatar.url)
+    embed_msg.timestamp = msgobj.created_at
 
-        await ctx.channel.send(embed=embedVar)
+    await ctx.channel.send(embed=embed_msg)
 
-        
-
-#@bot.command(name="rimg")
-#sync def reddit_img(ctx, subreddit):
-#await bot.process_commands(message)
-        
 
 @bot.command()
 async def img(ctx, subreddit):
-        http = urllib3.PoolManager()
-        url = "https://reddit.com/r/" + subreddit + "/top.json"
-        bruh = http.request('GET', url, timeout = 2.0)
-        jsonData = json.loads(bruh.data.decode('utf-8'))
+    http = urllib3.PoolManager()
+    url = "https://reddit.com/r/" + subreddit + "/top.json"
+    bruh = http.request('GET', url, timeout = 2.0)
+    json_data = json.loads(bruh.data.decode('utf-8'))
 
-        try:
-            number_of_posts = jsonData['data']['dist']
-        except KeyError:
-            await ctx.channel.send("Too many requests, try again later")
-            return
+    try:
+        number_of_posts = json_data['data']['dist']
+    except KeyError:
+        await ctx.channel.send("Too many requests, try again later")
+        return
 
-        image_posts = [];
-        for i in range(0, number_of_posts):
-            post_data = jsonData['data']['children'][i]['data']
-            img_title = post_data['title']
-            img_url = post_data['url']
-            if img_url.endswith('.png') or img_url.endswith('.jpg') or img_url.endswith('.gif'):
-                image_posts.append((img_title, img_url))
-            elif "gallery" in img_url:
-                media_id = post_data['gallery_data']['items'][0]['media_id']
-                img_url = post_data['media_metadata'][media_id]['s']['u']
-                img_url = img_url.replace("preview", "i", 1)
-                img_url = img_url[:img_url.index('?')]
-                image_posts.append((img_title, img_url))
+    image_posts = [];
+    for i in range(0, number_of_posts):
+        post_data = json_data['data']['children'][i]['data']
+        img_title = post_data['title']
+        img_url = post_data['url']
+        if img_url.endswith('.png') or img_url.endswith('.jpg') or img_url.endswith('.gif'):
+            image_posts.append((img_title, img_url))
+        elif "reddit.com/gallery" in img_url:
+            media_id = post_data['gallery_data']['items'][0]['media_id']
+            img_url = post_data['media_metadata'][media_id]['s']['u']
+            img_url = img_url.replace("preview", "i", 1)
+            img_url = img_url[:img_url.index('?')]
+            image_posts.append((img_title, img_url))
 
-        if (len(image_posts) == 0):
-            await ctx.channel.send(f"Top posts of r/{subreddit} do not have images\n")
-            return
+    if (len(image_posts) == 0):
+        await ctx.channel.send(f"Top posts of r/{subreddit} do not have images")
+        return
 
-        random_post = random.choice(image_posts)
-        embedVar = discord.Embed(title=random_post[0])
-        embedVar.set_image(url=random_post[1])
-        embedVar.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar)
-        await ctx.channel.send(embed=embedVar)
+    random_post = random.choice(image_posts)
+    embed_msg = discord.Embed(title=random_post[0])
+    embed_msg.set_image(url=random_post[1])
+    embed_msg.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar)
+    await ctx.channel.send(embed=embed_msg)
 
 
 @bot.command(help="sends a random pinned message of user")
-async def pinned(ctx, user : discord.Member):
+async def pinned(ctx, user: discord.Member):
     pinned_messages = await ctx.channel.pins()
     user_messages = []
     for message in pinned_messages:
@@ -143,15 +133,15 @@ async def pinned(ctx, user : discord.Member):
         await ctx.channel.send("User has no pinned messages in this channel")
         return
 
-    msgobj = random.choice(user_messages)
+    random_msg = random.choice(user_messages)
 
-    embedVar = discord.Embed(title="Jump to message", description=msgobj.content, url=msgobj.jump_url)
-    if msgobj.attachments:
-        embedVar.set_image(url=msgobj.attachments[0].url)
-    embedVar.timestamp = msgobj.created_at
-    embedVar.set_footer(text=user.name, icon_url=user.avatar)
+    embed_msg = discord.Embed(title="Jump to message", description=random_msg.content, url=random_msg.jump_url)
+    if random_msg.attachments:
+        embed_msg.set_image(url=random_msg.attachments[0].url)
+    embed_msg.timestamp = random_msg.created_at
+    embed_msg.set_footer(text=user.name, icon_url=user.avatar)
 
-    await ctx.channel.send(embed=embedVar)
+    await ctx.channel.send(embed=embed_msg)
 
 
 DISCORD_TOKEN = os.getenv('TOKEN')
